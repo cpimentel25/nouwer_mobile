23d44ede8dc485853c442767441a123d43dde3b1
CP <cpimentel.0325@gmail.com>
2023-04-01 10:00:44 -0500

    fix undefined list categories by roster

--- src/Components/Roster/Select/style.js
+++ src/Components/Roster/Select/style.js
@@ -1,15 +1,30 @@
 import { StyleSheet } from "react-native";
+import { Colors } from "../../../Styles";
 
 const styles = StyleSheet.create({
   selectRoster: {
     width: 400,
-    fontSize: 40,
-    borderRadius: 3,
-    marginBottom: 20,
+    height: 40,
+    marginTop: 30,
+    marginBottom: 30,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  title: {
+    // width: '15%',
+    color: Colors.text,
+    fontSize: 15,
+  },
+  selectOption: {
+    width: '85%',
+    flexDirection: 'row',
+    // borderBottomColor: Colors.textUp,
+    // borderBottomWidth: 1,
   },
   option: {
-    backgroundColor: '#E4DCCF',
-    borderRadius: 3,
+    width: '100%',
+    color: Colors.textUp,
   }
 });
 
